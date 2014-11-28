90240185134db721d6c3f7fff771518308e833f1
Hemant Kumar <hemantksingh.hk@gmail.com>
2014-11-28 00:09:35 +0000

    Added named pipes for inter process communication.

---
+++ NamedPipes/NamedPipeClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace NamedPipes
+{
+    public class NamedPipeClient
+    {
+        private readonly ChannelFactory<IProducingDocumentPipe> _channelFactory;
+
+        public NamedPipeClient(string documentName)
+        {
+            string uriString = string.Format("net.pipe://localhost/ProducingDocument/{0}/{1}", Environment.UserName,
+                documentName.ToLower());
+            _channelFactory = new ChannelFactory<IProducingDocumentPipe>(new NetNamedPipeBinding(), uriString);
+        }
+
+        public bool IsProducing()
+        {
+            try
+            {
+                _channelFactory.Open();
+                IProducingDocumentPipe channel = _channelFactory.CreateChannel();
+                return channel.IsProducing();
+            }
+            catch (EndpointNotFoundException)
+            {
+                _channelFactory.Abort();
+                return false;
+            }
+            finally
+            {
+                _channelFactory.Close();
+            }
+        }
+    }
+
+    [ServiceContract]
+    public interface IProducingDocumentPipe
+    {
+        [OperationContract]
+        bool IsProducing();
+    }
+}
---
+++ NamedPipes/NamedPipeServer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace NamedPipes
+{
+    /// <summary>
+    /// Named pipes provide interprocess communication between a pipe server and one or more pipe clients. 
+    /// They offer more functionality than anonymous pipes, which provide interprocess communication on a 
+    /// local computer. Named pipes support full duplex communication over a network and multiple server 
+    /// instances, message-based communication, and client impersonation, which enables connecting processes 
+    /// to use their own set of permissions on remote servers.
+    /// </summary>
+    public class NamedPipeServer : IDisposable
+    {
+        private readonly string _documentName;
+        private ServiceHost _serviceHost;
+
+        public NamedPipeServer(string documentName)
+        {
+            _documentName = documentName;
+            OpenNamedPipe();
+        }
+
+        private void OpenNamedPipe()
+        {
+            string uriString = string.Format("net.pipe://localhost/ProducingDocument/{0}/{1}", Environment.UserName,
+                _documentName.ToLower());
+            var wordAddIn = new Uri(uriString);
+            var addinCommunicationService = new ProducingDocumentPipe();
+            _serviceHost = new ServiceHost(addinCommunicationService, wordAddIn);
+            _serviceHost.Open();
+        }
+
+        public void CloseNamedPipe()
+        {
+            _serviceHost.Close();
+        }
+
+        public void Dispose()
+        {
+            CloseNamedPipe();
+        }
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, UseSynchronizationContext = false)]    
+    public class ProducingDocumentPipe : IProducingDocumentPipe
+    {
+        public bool IsProducing()
+        {
+            return true;
+        }
+    }
+}
